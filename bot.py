e3b30ce47a4dbec809498560d5e1a22e9e63b73e
Yumahau <37306698+Yumahau@users.noreply.github.com>
2025-04-29 16:32:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,24 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from handlers import register_handlers
+from dotenv import load_dotenv
 import os
+from handlers import register_handlers
 
-TOKEN = os.getenv("BOT_TOKEN") or "716023088:AAGpwBdCKZjcFQ21qE7jowaXps2_zIVbVWw"
+# Загружаем переменные окружения
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def main():
+    # Инициализация бота
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
+
+    # Регистрация всех хендлеров
     register_handlers(dp)
+
+    # Запуск long-polling
     await dp.start_polling(bot)
 
+# Точка входа
 if __name__ == "__main__":
     asyncio.run(main())
