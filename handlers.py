5bfe1d5249e9d0963a800b54a7a7938f16696b3e
Yumahau <37306698+Yumahau@users.noreply.github.com>
2025-04-29 17:41:37 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,52 +1,15 @@
-# handlers.py
-
 from aiogram import types
-from db import add_task, get_tasks, get_executors
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from db import get_executors
 
-# Регистрация хендлеров
-def register_handlers(dp):
-    dp.register_message_handler(start_handler, commands=["start"])
-    dp.register_message_handler(new_task_handler, commands=["new_task"])
-    dp.register_message_handler(tasks_handler, commands=["tasks"])
-    dp.register_message_handler(send_tasks_handler, commands=["send_tasks"])
+async def register_handlers(dp):
+    @dp.message_handler(commands=['start'])
+    async def handle_start(message: types.Message):
+        await on_start(message)
 
-# Команда /start
-async def start_handler(message: types.Message):
-    await message.answer("Привет! Отправь мне новое задание!")
+    @dp.message_handler(commands=['tasks'])
+    async def handle_tasks(message: types.Message):
+        await handle_tasks(message)
 
-# Команда для создания нового задания
-async def new_task_handler(message: types.Message):
-    # Пример задания (можно сделать ввод через несколько шагов)
-    user_id = message.from_user.id
-    username = message.from_user.username
-    description = "Задание по ремонту"
-    price = 500.0
-
-    # Добавление задания в базу данных
-    await add_task(user_id, username, description, price)
-    await message.answer("Задание добавлено!")
-
-# Команда для просмотра всех заданий
-async def tasks_handler(message: types.Message):
-    tasks = await get_tasks()
-    if tasks:
-        for task in tasks:
-            task_info = f"Задание: {task[2]}\nЦена: {task[3]} рублей\n\n"
-            await message.answer(task_info)
-    else:
-        await message.answer("Нет заданий.")
-
-# Команда для рассылки задания исполнителям
-async def send_tasks_handler(message: types.Message):
-    tasks = await get_tasks()
-    if tasks:
-        executors = await get_executors()
-        for task in tasks:
-            task_info = f"Задание: {task[2]}\nЦена: {task[3]} рублей\n\n"
-            for executor in executors:
-                # Отправка задания каждому исполнителю
-                await message.bot.send_message(executor, task_info)
-        await message.answer("Задания отправлены всем исполнителям!")
-    else:
-        await message.answer("Нет заданий для отправки.")
+    @dp.message_handler(commands=['new_task'])
+    async def handle_new_task(message: types.Message):
+        await handle_new_task(message)
